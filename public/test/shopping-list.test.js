134c75c56b8dd814549d30e39c0aa91dd0a7fcb0
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-16 15:14:56 -1000

    items initializes as empty array

--- public/test/shopping-list.test.js
+++ public/test/shopping-list.test.js
@@ -1,10 +1,14 @@
 let bar = new ShoppingList();
 
-describe('ShoppingList', function(){
-  it ('ShoppingList should be a class', function(){
+describe('ShoppingList', function() {
+  it('ShoppingList should be a class', function() {
     expect(bar).to.be.instanceOf(ShoppingList);
-  })
-  it ('ShoppingList should contain property "items"', function(){
+  });
+  it('ShoppingList should contain property "items"', function() {
     expect(bar).to.haveOwnProperty('items');
-  })
-})
+  });
+
+  it('ShoppingList Items should initialize as empty array', function() {
+    expect(bar.items.length).to.equal(0);
+  });
+});
